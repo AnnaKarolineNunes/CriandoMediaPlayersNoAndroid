61c901ad770c4ca7c33d57f6f801b8bd5fce350d
AnnaKarolineNunes <annamiranda894@gmail.com>
2024-01-24 20:08:14 -0300

    start commit

---
+++ app/src/main/java/com/cursoandroid/mediaplayer/MainActivity.java
@@ -0,0 +1,27 @@
+package com.cursoandroid.mediaplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // configurando a musica
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.teste);
+    }
+
+    // passo 2: criar o evento de clique
+    public void executarSom(View view){
+        if(mediaPlayer != null){
+            mediaPlayer.start();
+        }
+    }
+}
